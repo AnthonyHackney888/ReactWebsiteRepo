e21277bf9af5304f7d330fa29d965dd4afd2ff1f
AnthonyHackney <56259412+AnthonyHackney@users.noreply.github.com>
2022-03-11 10:59:19 -0500

    initial commit

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,4 +2,3 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as InfoSection } from './InfoSection/InfoSection';
 export {default as Footer} from './Footer/Footer';
 export {default as EmailForm} from './EmailForm/EmailForm';
-//export {default as Resume} from './Resume'
